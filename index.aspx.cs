cbccd1cfa66abf7722f20cc13e01281b7fa5763a
Sindhupriya30 <163632709+Sindhupriya30@users.noreply.github.com>
2026-02-06 13:32:55 +0530

    Add files via upload

---
+++ index.aspx.cs
@@ -0,0 +1,8 @@
+﻿/* Previous value was gap: 8px; */
+.game-board {
+    display: grid;
+    gap: 0px; /* <--- THIS IS THE CHANGE: No space between grid items */
+    margin: 0 auto;
+    justify-content: center;
+    perspective: 1000px;
+}
